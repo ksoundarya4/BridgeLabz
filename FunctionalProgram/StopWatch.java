15f520592a1ca9efdd70df6a199e21156c7efb14
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-23 17:23:42 +0530

    [soundarya]Refactor. Added file and function comment

--- FunctionalProgram/StopWatch.java
+++ FunctionalProgram/StopWatch.java
@@ -1,23 +1,47 @@
+/******************
+ * Logical Programs
+ * purpose : Stopwatch Program for measuring the time 
+ *           that elapses between the start and end clicks
+ * @discription
+ * @file StopWatch.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ * @since 11/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
-
 public class StopWatch {
 
 	private long startWatch;
 	private long stopWatch;
 	private boolean running;
 	
+	/**
+	 * Function to start StopWatch 
+	 * @param{long}startWatch - stores starting time
+	 * @return - nothing
+	 */
 	public  void start()
 	{
 		startWatch = System.currentTimeMillis();
 		running = true;
 	}
 	
+	/**
+	 * Function to Stop StopWatch
+	 * @param{long}stopWatch - stores stopping time
+	 * @return - nothing
+	 */
 	public  void stop()
 	{
 		stopWatch = System.currentTimeMillis();
 		running = false;
 	}
 	
+	/**
+	 * Function to calculate Elapsed time
+	 * @param{long}elapsed - calculates time elapsed
+	 * @return - returns time elapsed
+	 */
 	public long GetElapsed()
 	{
 		
