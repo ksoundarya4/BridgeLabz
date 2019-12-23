5b89e62571554d4a46471601df0bec1c4a17e693
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-23 16:28:31 +0530

    [soundarya]Refactor. Added file and function comment

--- FunctionalProgram/WindChill.java
+++ FunctionalProgram/WindChill.java
@@ -1,7 +1,22 @@
+/***************
+ * Functional Program
+ * purpose : Takes double command-line argument 
+ * t and v and prints wind chill.
+ * @discription
+ * @file WindChill.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ * @since 10/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
-
 public class WindChill {
 	
+	/**
+	 * Calculates Wind Chill
+	 * @param{double}t - Temperature t (in Fahrenheit)
+	 * @param{double}v - wind speed (in miles per hour)
+	 * @return{double}w - Effective Temperature(wind chill)
+	 */
 	public static double calulateWindChill(double t , double v)
 	{
 		double V = Math.pow(v,0.16);
@@ -9,6 +24,11 @@ public class WindChill {
 		return w;
 	}
 	
+	/**
+	 * To accept command-line arguments
+	 * @param{String} args[0] - input  temperature in degree fahrenheit
+	 * @param{String} args[1] - input  velocity in miles per second
+	 */
 	public static void main(String[] args)
 	{
 		double temp = 0 ;
