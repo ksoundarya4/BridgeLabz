855ff5343d6cea7591a5a49bd76dba3e5846f40c
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-24 11:04:00 +0530

    [soundarya]Refactor. Added file comment and function comments

--- FunctionalProgram/CouponNumber.java
+++ FunctionalProgram/CouponNumber.java
@@ -1,8 +1,22 @@
+/**********************
+ *Logical Program
+ *purpose : To count random numbers generated to 
+ *          generate distinct coupons.
+ * @discription
+ * @file CouponNumber.java 
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ *@since 24/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
 public class CouponNumber {
-	
-	
-	public static int generateCoupon(int N){
+
+	/**
+	 * To generate distinct coupons
+	 * @param{int}N - N distinct coupon numbers
+	 * @return{int} count - random number required
+	 */
+	 public static int generateCoupon(int N){
 		int count = 0;
 		int distinct = 0;
 		boolean[] iscollected = new boolean[N];
