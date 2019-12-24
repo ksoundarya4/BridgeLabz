08cd03defb03b148fe4aabb58194265057ea69dd
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-24 12:19:33 +0530

    [soundarya]Refactor. Added file comments and function comments

--- FunctionalProgram/Binary.java
+++ FunctionalProgram/Binary.java
@@ -1,59 +1,62 @@
+/*****************
+ * JUNIT Program
+ * purpose : To convert a decimal to binary digit, 
+ *           To swap nibbles and find new number
+ *           Find new number is a power of 2
+ * @discription
+ * @file Binary.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ * @since 14/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
+public class Binary {
 
-
-  public class Binary {
-
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Enter a number to be converted");
 		int num = utilclass.InputInteger();
 	
-		//To obtain binary of integer
-	int[] binary = utilclass.toBinary(num);
-	//To print binary digits of number
-	for(int i =0; i<binary.length; i++)
-	{
-		System.out.print(binary[i]);
-	}
+		
+	    int[] binary = utilclass.toBinary(num);
+        for(int i =0; i<binary.length; i++)
+	    {
+		   System.out.print(binary[i]);
+	    }
 	
-	//To swap nibbles in number
-	int swapNibble = swapNibble(num);
-	System.out.println();
-	System.out.println("Number after swaping nibbles = "+swapNibble);
+	     int swapNibble = swapNibble(num);
+	     System.out.println();
+	     System.out.println("Number after swaping nibbles = "+swapNibble);
 	
+	     if(isPowerOf2(swapNibble))
+	    	 System.out.println("The swapped number "+swapNibble+ " is power of 2");
+         if(!isPowerOf2(swapNibble))
+        	 System.out.println("The swapped number "+swapNibble+ " is  not power of 2");
+ }
 	
-	//To check if swapped number is a power of 2
-	if(isPowerOf2(swapNibble))
-	System.out.println("The swapped number "+swapNibble+ " is power of 2");
-
-	if(!isPowerOf2(swapNibble))
-		System.out.println("The swapped number "+swapNibble+ " is  not power of 2");
-}
-	
-	
-	//Method to swap nibbles
-	public static int swapNibble(int num)
-	{
+	/**
+	 * To swap nibble of a number
+	 * @param{int}num - Input number to swap its nibbles
+	 * @return{int} Resultant number
+	 */
+	public static int swapNibble(int num) {
 		return ((num & 0x0f) << 4) | ((num & 0xf0) >> 4);
-	
 	}
 	
-	//Method to check is power of two
-	public static boolean isPowerOf2(int num)
-	{
+	/**
+	 * To check the resultant number is power of 2
+	 * @param{int}num - Resultant number from swapNibbles
+	 * @return{boolean} true if it is power of 2
+	 */
+	public static boolean isPowerOf2(int num) {
+		
 		if(num == 0)
 			return false;
 		while(num!=1)
 		{
 			if(num%2==1)
 				return false;
-			num=num/2;
+			num=num/2; 
 		}
 		return true;
 	}
-		
-		
-
-	
-
 }
