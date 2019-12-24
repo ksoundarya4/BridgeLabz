7e1accd9b1bdf4a70246d79027c0f9448f06029a
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-24 09:39:18 +0530

    [soundarya]Refactor. Added file comments and function comments

--- FunctionalProgram/VendingMachine.java
+++ FunctionalProgram/VendingMachine.java
@@ -1,5 +1,15 @@
+/*********************
+ * JUNIT programs
+ * purpose : to calculate the minimum number of Notes as
+ *           well as the Notes to be returned by the Vending 
+ *           Machine as a Change
+ *  @discription
+ *  @fie VendingMachine.java
+ *  @author soundarya<ksoundarya4>
+ *  @version 1.0
+ *  @since 11/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
-
 public class VendingMachine {
 
 	//To Determine the available notes in Vending Machine
@@ -10,8 +20,14 @@ public class VendingMachine {
 	//To check total number of notes returned
 	private static int TotalNotes = 0;
 	
-	public static int calculate(int Money , int[] notes)
-	{
+	/**
+	 * To calculate number of notes to be returned
+	 * @param{int}Money - To be returned by VendingMachine
+	 * @param{int[]}notes - Integer array of available notes in the machine
+	 * @return{int}number - Number of notes returned from Machine 
+	 */
+	public static int calculate(int Money , int[] notes) {
+		
 		int reminder = 0;
 		if(Money==0)
 			return -1;
