170cecc11bd1f50580ebf95195e2db17627056c6
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-24 17:18:04 +0530

    [soundarya]Refactor. Added file and function comments

--- FunctionalProgram/Array2D.java
+++ FunctionalProgram/Array2D.java
@@ -1,7 +1,23 @@
-
+/***********************
+ * Functional Programs
+ * purpose :A library for reading in 2D arrays of integers, 
+ *          doubles, or booleans from standard input and
+ *          printing them out to standard output.
+ * @discription
+ * @file Array2D.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0        
+ * @since 12/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
+
 public class Array2D<T> {
 
+	/**
+	 * To read the number of rows
+	 * 
+	 * @return{int} rows
+	 */
 	public static int getRows() {
 
 		System.out.println("Enter number of rows");
@@ -9,6 +25,11 @@ public class Array2D<T> {
 		return rows;
 	}
 
+	/**
+	 * To read the number of columns
+	 * 
+	 * @return{int} columns
+	 */
 	public static int getColms() {
 
 		System.out.println("Enter number of coloums");
@@ -16,6 +37,13 @@ public class Array2D<T> {
 		return colms;
 	}
 
+	/**
+	 * To read Integer 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 * @param{Integer}arr2D - Array to be read @return{Integer[][]} 2D Integer Array
+	 */
 	private static Integer[][] readInteger2dArray(int rows, int colms, Integer[][] arr2D) {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < colms; j++) {
@@ -26,6 +54,13 @@ public class Array2D<T> {
 		return arr2D;
 	}
 
+	/**
+	 * To read Double 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 * @param{Double}arr2D - Array to be read @return{Double[][]} 2D Double Array
+	 */
 	public static Double[][] readDouble2dArray(int rows, int colms, Double[][] arr2D) {
 
 		for (int i = 0; i < rows; i++) {
@@ -37,6 +72,13 @@ public class Array2D<T> {
 		return arr2D;
 	}
 
+	/**
+	 * To read Boolean 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 * @param{Boolean}arr2D - Array to be read @return{Boolean[][]} 2D Boolean Array
+	 */
 	private static Boolean[][] readBoolean2dArray(int rows, int colms, Boolean[][] arr2D) {
 
 		for (int i = 0; i < rows; i++) {
@@ -48,6 +90,13 @@ public class Array2D<T> {
 		return arr2D;
 	}
 
+	/**
+	 * Generic method to print 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 * @param{T}arr2D - Array to be print
+	 */
 	public static <T> void print2dArray(int rows, int colm, T[][] arr) {
 
 		for (int i = 0; i < rows; i++) {
@@ -57,6 +106,12 @@ public class Array2D<T> {
 		}
 	}
 
+	/**
+	 * To read and print Integer 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 */
 	public static void integer2dArray(int rows, int colms) {
 
 		Integer[][] arr2D = new Integer[rows][colms];
@@ -66,6 +121,12 @@ public class Array2D<T> {
 		print2dArray(rows, colms, arr2D);
 	}
 
+	/**
+	 * To read and print Double 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 */
 	public static void double2DArray(int rows, int colms) {
 
 		Double[][] arr2D = new Double[rows][colms];
@@ -76,6 +137,12 @@ public class Array2D<T> {
 
 	}
 
+	/**
+	 * To read and print Boolean 2D Array
+	 * 
+	 * @param{int}rows - number of rows Array has
+	 * @param{int}colms - number of columns Array has
+	 */
 	public static void boolean2DArray(int rows, int colms) {
 
 		Boolean[][] arr2D = new Boolean[rows][colms];
@@ -86,8 +153,16 @@ public class Array2D<T> {
 
 	}
 
+	/**
+	 * To select which 2D array need to be executed
+	 * 
+	 * @param{String}s - input which 2Darray to be executed
+	 *                 "IntegerArray","DoubleArray","BooleanArray"
+	 * @param{int}rows - Number of rows a Array has.
+	 * @param{int}colms -Number of columns a Array has.
+	 */
 	public static void SelectArray(String s, int rows, int colms) {
-		
+
 		if (s.equals("IntegerArray"))
 			integer2dArray(rows, colms);
 
