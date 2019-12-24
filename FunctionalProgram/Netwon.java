4021d09d5629513f718433fcbfb12ea7ebfcf017
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-24 11:35:01 +0530

    [soundarya]Refactor. Added file comments and function comments

--- FunctionalProgram/Netwon.java
+++ FunctionalProgram/Netwon.java
@@ -1,5 +1,14 @@
+/******************
+ * JUNIT Program
+ * purpose : to compute the square root of a nonnegative number 
+ *           using Newton's method.
+ * @discription
+ * @file Newton.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ * @since 12/12/2019
+ */
 package com.bridgelabs.FunctionalProgram;
-
 public class Netwon {
 
 	public static void main(String[] args) {
