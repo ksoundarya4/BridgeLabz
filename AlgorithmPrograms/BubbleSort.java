aebc79bb97a95dd94d31f87e874f7810b7ad88cc
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-26 17:31:11 +0530

    [soundarya]Refactor. Refactored the code

--- AlgorithmPrograms/BubbleSort.java
+++ AlgorithmPrograms/BubbleSort.java
@@ -1,35 +1,39 @@
-
+/****************
+ * Algorithm Program
+ * purpose :  Reads in integers prints them in sorted order using Bubble Sort
+ * @discription
+ * @file BubbleSort.java
+ * @author soundarya<ksoundarya4>
+ */
 package com.bridgelabs.AlgorithmPrograms;
+
 public class BubbleSort {
-	
-	public static int[] Bubblesort(int[] arr )
-	{
-		for(int i =0; i<arr.length; i++)
-		{
-			for(int j = 0; j<arr.length-i-1; j++)
-			{
-				if(arr[j] > arr[j+1])
-				{
-				int	temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+
+	/**
+	 * To sort given Array using Bubble sort.
+	 * 
+	 * @param arr - Integer Array that need to be sorted.
+	 * @return sorted Integer Array
+	 */
+	public static int[] bubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 				}
 			}
-				
+
 		}
-	return arr;
+		return arr;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("Enter  10 integer numbers to sort");
-	int[] a = new int[10];
-	//Accept integer input
-		for(int i = 0; i<a.length;i++)
-			a[i] = utilclass.InputInteger();
-		
-		a = Bubblesort(a);
-		for(int i = 0 ; i<a.length; i++)
-			System.out.print(a[i]+" ");
+
+		int[] a = utilclass.readIntegerArray();
+		a = bubbleSort(a);
+		utilclass.printArray(a);
 	}
 
 }
