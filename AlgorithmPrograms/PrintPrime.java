3c5cdd8118af29de24f2d981f56864213fc0a8dd
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-27 09:54:15 +0530

    [soundarya]Fix. Fixed print Prime code

--- AlgorithmPrograms/PrintPrime.java
+++ AlgorithmPrograms/PrintPrime.java
@@ -1,21 +1,50 @@
 package com.bridgelabs.AlgorithmPrograms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintPrime {
 
-	public static void main(String[] args) {
-//	List<String> l = new ArrayList<String>();
-//	String[] str = null;
-	for(int i = 0; i<=1000; i++)
-		{
-		//To find prime numbers in 0-1000 range
-			//and to check palindrome
-			
-			String s = Integer.toString(i);
-			
-		if(utilclass.isPrime(i) && utilclass.isPalindrome(s))
-		System.out.println(i);
+	public static List<String> primeBetween0To1000() {
+		List<String> l = new ArrayList<String>();
+
+		for (int i = 0; i <= 1000; i++) {
+			if (utilclass.isPrime(i)) {
+				String s = Integer.toString(i);
+				System.out.println(i);
+				l.add(s);
+			}
+		}
+		return l;
+	}
+
+	public static void primeAnagram(List<String> l) {
+		for (int i = 0; i < l.size(); i++) {
+			for (int j = i + 1; j < l.size(); j++) {
+				if (utilclass.isAnagram(l.get(i), l.get(j)))
+					System.out.println(l.get(i) + " and " + l.get(j));
+			}
+		}
+	}
 	
-		
+	public static void primePalindrome(List<String> l) {
+		for(String str : l)
+		{
+			if(utilclass.isPalindrome(str))
+				System.out.println(str);
 		}
 	}
 
+	public static void main(String[] args) {
+		
+		System.out.println("Prime Numbers between 1 tp 1000 are : ");
+		List<String> l = primeBetween0To1000();
+		
+		System.out.println("Prime that are Anagram : ");
+		primeAnagram(l);
+		
+		System.out.println("Prime that are Palindrome : ");
+		primePalindrome(l);
+
+	}
 }
