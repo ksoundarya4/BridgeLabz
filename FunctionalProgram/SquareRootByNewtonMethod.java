d5ad9531d0e5879bc077c76e43a95c83536464f5
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-28 10:49:32 +0530

    [soundarya]Add. Added SquareRootByNewtonMethod

---
+++ FunctionalProgram/SquareRootByNewtonMethod.java
@@ -0,0 +1,23 @@
+/******************
+ * JUNIT Program
+ * 
+ * @purpose  To compute the square root of a nonnegative number 
+ *           using Newton's method.
+ * @file Newton.java
+ * @author soundarya<ksoundarya4>
+ * @version 1.0
+ * @since 12/12/2019
+ */
+package com.bridgelabs.FunctionalProgram;
+
+public class SquareRootByNewtonMethod {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number to find  square root");
+		int num = utilclass.InputInteger();
+
+		System.out.println("Square root of " + num + " = " + utilclass.sqrt(num));
+
+	}
+
+}
